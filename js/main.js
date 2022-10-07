8147630dda5440a752ea3dea1b6f29f8983d0a1c
Justam23 <fedorov@unicorn39.ru>
2022-10-07 19:52:12 +0200

    Add 2 functions to main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// © https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// © https://github.com/htmlacademy-javascript/237031-kekstagram-simple-27/pull/2/files
+let checkStringLength = (string, maxLength) => string.length <= maxLength;
+checkStringLength ('test test test', 10);
